8ad42ceec083bfd2786308b557c5c6a954af48cc
Arbi Bashaev <arbi.personal@gmail.com>
2018-11-30 17:50:52 +0300

    Added extensions

--- GrillBox/GrillBox/ViewController.swift
+++ GrillBox/GrillBox/ViewController.swift
@@ -8,13 +8,47 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+struct Post: Codable {
+    var userId: Int
+    var id: Int
+    var title: String
+    var body: String
+}
 
+
+
+class ViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        let myView = UIView()
+        myView.backgroundColor = .red
+        view.addSubview(myView)
+        myView.fillSuperview()
+        //        let myPost = Post(userId: "1", id: "MyID", title: "MyTitle", body: "MyPostBody")
+        
+        let urlString = "https://jsonplaceholder.typicode.com/posts"
+        guard let url = URL(string: urlString) else { return }
+        
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            guard let data = data else { return }
+            guard error == nil else { return }
+            
+            do {
+                let posts = try JSONDecoder().decode([Post].self, from: data)
+                
+                print(posts)
+            } catch {
+                print(error)
+            }
+            }.resume()
     }
-
-
+    
+    
 }
+
+
+
 
