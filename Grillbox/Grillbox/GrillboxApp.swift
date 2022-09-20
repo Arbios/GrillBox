c2bbca7317b2528eb89a2460adc1f7196efc2543
Arbi Bashaev <a.bashaev@getrito.com>
2022-09-21 01:36:12 +0300

    Remove using pods

---
+++ Grillbox/Grillbox/GrillboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  GrillboxApp.swift
+//  Grillbox
+//
+//  Created by Admin on 21.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct GrillboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
